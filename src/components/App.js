3df05e3f38e619ddf085e13da41ebf9caf3b3198
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-02-10 11:50:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,21 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
+ const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
 
+  useEffect(() => {
+    setOutputValue(inputValue ** 2);
+  }, [inputValue]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+ 
   return (
     <div id="main">
       <ChildComponent /> 
-       <input id='input' onChange={} />
+       <input id='input' value={inputValue} onChange={handleInputChange} />
     
       <p id='output'>{}</p>
     </div>
